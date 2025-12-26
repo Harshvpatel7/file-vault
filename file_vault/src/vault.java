183609cb46d67562cfe22cc77403d62b95cf3732
Harshvpatel7 <harshpatel58181@gmail.com>
2025-12-26 18:23:37 +0530

    FILE INITIALIZATION

---
+++ file_vault/src/vault.java
@@ -0,0 +1,25 @@
+package file_vault.src;
+
+// public class vault {
+//     public static void main(String[] args) {
+//         if(args.length == 0){
+//             System.out.println("executed");
+//         }else {
+//             System.out.println("You provided: " + args[0]);
+//         }
+        
+//     }
+// }
+public class vault {
+    public static void main(String[] args) {
+        
+
+        if(args.length == 0){
+            help();
+        }
+    }
+
+    public static void help(){
+        System.out.println("");
+    }
+}
